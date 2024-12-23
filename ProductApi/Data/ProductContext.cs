4bcbf0bbf310adf2983bfa612e3f42ae3d0a9c86
manasjainbit <40699150+manasjainbit@users.noreply.github.com>
2024-12-23 20:34:37 +0530

    Add files via upload

---
+++ ProductApi/Data/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Models; 
+using System.Data.Entity;
+
+namespace ProductApi.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ProductApi/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add Entity Framework Core with an In-Memory Database
+builder.Services.AddDbContext<ProductContext>(options =>
+    options.UseInMemoryDatabase("ProductDb"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
